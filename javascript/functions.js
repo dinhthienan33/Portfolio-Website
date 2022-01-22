eaef8070389e186fa214dc3b547a37a7a26fe9f6
Andrew Krause <aek.krause@gmail.com>
2022-01-22 17:00:55 -0600

    Finished contact page. Still need to finish modals in project page. Then will begin making it a desktop view. Mobile view practically complete.

--- javascript/functions.js
+++ javascript/functions.js
@@ -10,23 +10,32 @@
 // Offset this script until all of the HTML content is loaded
 // document.addEventListener("DOMContentLoaded", start);
 
-// Create an array of objects to list out the projects on the website. --> WILL LIKELY COMPLETE LATER
-let computerProjects = [
-    {},
-    {},
-]
+// // Create an array of objects to list out the projects on the website. --> WILL LIKELY COMPLETE LATER
+// let computerProjects = [
+//     {},
+//     {},
+// ]
 
 // --> SECTION: Style the links in the navbar.
 
 // Get the elements in the navbar by their ids.
 document.getElementById("menu-about").addEventListener("click", about);
+document.getElementById("menu-resume").addEventListener("click", resume);
 document.getElementById("menu-projects").addEventListener("click", projects);
 document.getElementById("menu-contact").addEventListener("click", contact);
 
 // Add a style (a highlight) to the "About" button if it is clicked.
 function about() {
-  // document.getElementById("menu-about").classList.remove("is-active");
   document.getElementById("menu-about").classList.add("is-active");
+  document.getElementById("menu-resume").classList.remove("is-active");
+  document.getElementById("menu-projects").classList.remove("is-active");
+  document.getElementById("menu-contact").classList.remove("is-active");
+}
+
+// Add a style (a highlight) to the "Resume" button if it is clicked.
+function resume() {
+  document.getElementById("menu-about").classList.remove("is-active");
+  document.getElementById("menu-resume").classList.add("is-active");
   document.getElementById("menu-projects").classList.remove("is-active");
   document.getElementById("menu-contact").classList.remove("is-active");
 }
@@ -34,7 +43,7 @@ function about() {
 // Add a style (a highlight) to the "Projects" button if it is clicked.
 function projects() {
   document.getElementById("menu-about").classList.remove("is-active");
-  // document.getElementById("menu-projects").classList.remove("is-active");
+  document.getElementById("menu-resume").classList.remove("is-active");
   document.getElementById("menu-projects").classList.add("is-active");
   document.getElementById("menu-contact").classList.remove("is-active");
 }
@@ -42,8 +51,8 @@ function projects() {
 // Add a style (a highlight) to the "Contact" button if it is clicked.
 function contact() {
   document.getElementById("menu-about").classList.remove("is-active");
+  document.getElementById("menu-resume").classList.remove("is-active");
   document.getElementById("menu-projects").classList.remove("is-active");
-  // document.getElementById("menu-contact").classList.remove("is-active");
   document.getElementById("menu-contact").classList.add("is-active");
 }
 
